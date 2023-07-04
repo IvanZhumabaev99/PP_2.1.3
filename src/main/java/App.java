4e9dd348201badc679cb3d275b92616a1a04e3b2
IvanZhumabaev99 <107753325+IvanZhumabaev99@users.noreply.github.com>
2023-07-04 13:04:09 +0300

    fix: pp_2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld1 = applicationContext.getBean(HelloWorld.class);
+        HelloWorld helloWorld2 = applicationContext.getBean(HelloWorld.class);
+        System.out.println("HelloWorld Pair: " + (helloWorld1 == helloWorld2));
+
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+        System.out.println("Cat Pair: " + (cat1 == cat2));
+
     }
 }
